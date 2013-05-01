e04a7c580e88abd9ee2e06c05b07655daaf4fe1c
ryan bridges <ryan.bridges@turner.com>
2013-05-01 16:10:24 -0400

    adding basic save functionality to cloudant couchdb

--- routes/speedreport.js
+++ routes/speedreport.js
@@ -9,8 +9,39 @@ var childProcess = require('child_process')
 	, path = require('path')
 	, fs= require('fs')
 	, temp = require('temp')
-	, util = require('util');
+	, util = require('util')
+	, cradle = require('cradle')
+	, Url = require("url")
+	;
 
+cradle.setup({
+    host: process.env.CLOUDANT_URL||'localhost',
+    cache: true,
+    raw: false
+});
+var conn = new(cradle.Connection)
+	, db = conn.database('speedreport')
+;
+db.exists(function (err, exists) {
+	if (err) {
+		console.log('error connecting to database', err);
+	} else if (exists) {
+		console.log('speedreport database exists');
+	} else {
+		console.warn('speedreport database does not exists.');
+		console.log('creating database');
+		db.create();
+		/* populate design documents */
+		console.log('creating design docs');
+		db.save('_design/tests', {
+			all: {
+				map: function (doc) {
+				  if (doc.url) emit(doc.url, doc);
+				}
+			}
+		});
+	}
+});
 exports.index = function(req, res){
 	var url=req.param('url')||req.get('Referrer')||'http://www.cnn.com';
 	res.render('speedreport', {
@@ -40,8 +71,20 @@ exports.data = function(req, res){
 		  fs.close(info.fd, function(err) {
 		    fs.readFile(info.path, function (err, data) {
 			  if (err) throw err;
+			  var dataObj=(typeof data === 'object')?data:JSON.parse(data);
 			  res.set('Content-Type', 'application/json');
   			  res.send(data);
+				if(dataObj){
+					db.save(dataObj, function (err, res) {
+						if (err) {
+							console.error("our data object is empty!");
+						} else {
+							console.log("data was saved successfully", res);
+						}
+					});
+				}else{
+					console.error("our data object is empty!");
+				}
 			});
 		  });
 		})
