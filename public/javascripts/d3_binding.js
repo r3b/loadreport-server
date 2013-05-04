020775754cb3f8dbfa66936a8408b89423f174d0
ryan bridges <ryan.bridges@turner.com>
2013-05-03 23:59:31 -0400

    breakdown by property

--- public/javascripts/d3_binding.js
+++ public/javascripts/d3_binding.js
@@ -18,3 +18,25 @@ ko.bindingHandlers.sparkline = {
 
     }
 };
+ko.bindingHandlers.d3 = {
+    update:function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        //var options = allBindingsAccessor().sparklineOptions || {};
+        //jQuery(element).sparkline(value, options);
+        console.dir(value.data())
+        var data=value.data().map(function(x){return x[value.property]();});
+        jQuery(element).empty();
+        var x = d3.scale.linear()
+		    .domain([0, d3.max(data)])
+		    .range(["0px", "800px"]);
+        var chart=d3
+        	.select(element)
+        	.attr("class", "chart");
+        chart.selectAll("div")
+        	.data(data)
+  			.enter().append("div")
+    		.style("width", x)
+    		.text(function(d) { return d; });
+
+    }
+};
