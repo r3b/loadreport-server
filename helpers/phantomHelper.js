1444548e93d8b3ae5dcbc8ac7bf14766b4342153
ryan bridges <ryan.bridges@turner.com>
2013-05-05 21:56:19 -0400

    decoupled Phantom and DB logic from the router

--- helpers/phantomHelper.js
+++ helpers/phantomHelper.js
@@ -3,34 +3,87 @@ var childProcess = require('child_process')
 	, binPath = phantomjs.path
 	, path = require('path')
 	, fs= require('fs')
+	, cradle = require('cradle')
+	, Url = require("url")
+	, db_url=Url.parse(process.env.CLOUDANT_URL||'https://app15424114.heroku:ys8YAkD1RhvAoPRq5lyS73cQ@app15424114.heroku.cloudant.com')
+	, auth=db_url.auth.split(':')
+	, username=auth[0]||''
+	, password=auth[1]||''
 	, temp = require('temp')
-	, util = require('util');
+	, util = require('util')
+	, SPEED_REPORT_PATH=path.join(__dirname, '/../public/javascripts/speedreport.js');
 
-var url='http://www.cnn.com'
-	, task = 'performance'
-	, contentType='json';
-
-var childArgs = [
-  path.join(__dirname, '/../public/javascripts/speedreport.js'),
-  url
-]
-// Process the data (note: error handling omitted)
-temp.open('speedreport-', function(err, info) {
-  if(err) throw err;
-  	childArgs.push(info.path);
-	console.log("running \"%s\"", childArgs);
-	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-	  console.error(err);
-	  console.warn(stderr);
-	  console.log(stdout);
-	  fs.write(info.fd, stdout);
-	  fs.close(info.fd, function(err) {
-	    //console.dir(info);
-	    fs.readFile(info.path, function (err, data) {
-		  if (err) throw err;
-		  util.puts(data);
+console.log('couchdb host is %s',db_url.href);
+var conn = new(cradle.Connection)(db_url.hostname, 443, {
+			secure: true,
+			cache: true,
+    		raw: false,
+			auth: { username: username, password: password }
+		})
+	, db = conn.database('speedreport')
+;
+db.exists(function (err, exists) {
+	if (err) {
+		console.log('error connecting to database', err);
+	} else if (exists) {
+		console.log('speedreport database exists');
+	} else {
+		console.warn('speedreport database does not exists.');
+		console.log('creating database');
+		db.create();
+		/* populate design documents */
+		console.log('creating design docs');
+		db.save('_design/tests', {
+			all: {
+				map: function (doc) {
+				  if (doc.url) emit(doc.url, doc);
+				}
+			}
 		});
-	  });
-	  /**/
-	})
+	}
 });
+
+exports.speedReport=function(url,task,contentType, callback){
+	task=task||'performance';
+	contentType=contentType||'json';
+	var childArgs = [SPEED_REPORT_PATH, url];
+	// Process the data (note: error handling omitted)
+		temp.open('speedreport-', function(err, info) {
+		  if(err) return callback(err, null);
+		  	childArgs.push(info.path);
+			console.log("running \"%s\"", childArgs);
+			childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+			  fs.write(info.fd, stdout);
+			  fs.close(info.fd, function(err) {
+			    fs.readFile(info.path, function (err, data) {
+				  if (err) return callback(err, null);
+
+					if(data){
+						db.save(JSON.parse(data.toString()), function (err, res) {
+							if (err) {
+								console.error("there was an error saving the data", err);
+								return callback(err, null);
+							} else {
+								console.log("data was saved successfully", res);
+								return callback(null, res);
+							}
+						});
+					}else{
+						console.error("our data object is empty!");
+						return callback(new Error("the created object was empty"), null);
+					}
+				});
+			  });
+			})
+		});
+}
+
+exports.getSavedReport=function(id, callback){
+	db.get(id, function (err, doc) {
+		if(err){
+			callback(err, null);
+		}else{
+			callback(null, doc);
+		}
+	});
+}
--- routes/speedreport.js
+++ routes/speedreport.js
@@ -11,42 +11,8 @@ var childProcess = require('child_process')
 	, fs= require('fs')
 	, temp = require('temp')
 	, util = require('util')
-	, cradle = require('cradle')
-	, Url = require("url")
-	, db_url=Url.parse(process.env.CLOUDANT_URL||'https://app15424114.heroku:ys8YAkD1RhvAoPRq5lyS73cQ@app15424114.heroku.cloudant.com')
-	, auth=db_url.auth.split(':')
-	, username=auth[0]||''
-	, password=auth[1]||''
+	, phelper=require('../helpers/phantomHelper.js')
 	;
-console.log('couchdb host is %s',db_url.href);
-var conn = new(cradle.Connection)(db_url.hostname, 443, {
-			secure: true,
-			cache: true,
-    		raw: false,
-			auth: { username: username, password: password }
-		})
-	, db = conn.database('speedreport')
-;
-db.exists(function (err, exists) {
-	if (err) {
-		console.log('error connecting to database', err);
-	} else if (exists) {
-		console.log('speedreport database exists');
-	} else {
-		console.warn('speedreport database does not exists.');
-		console.log('creating database');
-		db.create();
-		/* populate design documents */
-		console.log('creating design docs');
-		db.save('_design/tests', {
-			all: {
-				map: function (doc) {
-				  if (doc.url) emit(doc.url, doc);
-				}
-			}
-		});
-	}
-});
 exports.index = function(req, res){
   res.render('index', { title: 'SpeedReport', path: '/speedreport/report' });
 };
@@ -66,37 +32,18 @@ exports.data = function(req, res){
 		, task = req.param('task')||'performance'
 		, contentType='json';
 	if(url){
-	var childArgs = [
-	  path.join(__dirname, '/../public/javascripts/speedreport.js'),
-	  url
-	]
-	// Process the data (note: error handling omitted)
-	temp.open('speedreport-', function(err, info) {
-	  if(err) throw err;
-	  	childArgs.push(info.path);
-		console.log("running \"%s\"", childArgs);
-		childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		  fs.write(info.fd, stdout);
-		  fs.close(info.fd, function(err) {
-		    fs.readFile(info.path, function (err, data) {
-			  if (err) throw err;
-			  res.set('Content-Type', 'application/json');
-  			  res.send(data);
-				if(data){
-					db.save(JSON.parse(data.toString()), function (err, res) {
-						if (err) {
-							console.error("there was an error saving the data", err);
-						} else {
-							console.log("data was saved successfully", res);
-						}
-					});
-				}else{
-					console.error("our data object is empty!");
-				}
-			});
-		  });
+		phelper.speedReport(url,task,contentType,function(err, data){
+			if(err){
+				res.send(500, err);
+			}else{
+				//check the database
+				phelper.getSavedReport(data._id, function (err, doc) {
+					res.set('Content-Type', 'application/json');
+		  			res.send(doc);
+				});
+
+			}
 		})
-	});
 	}else{
 		res.send(400, "invalid URL specified in search string");
 	}
