064d427f56759b8f7f41e21de4ccf605f78a8a0c
ryan bridges <ryan.bridges@turner.com>
2013-06-14 17:23:52 -0400

    Added Vagrantfiles for single- and multi-VM mode

--- Vagrant/cookbooks/loadreport/recipes/worker.rb
+++
@@ -1,8 +0,0 @@
-template '/etc/init/loadreport-worker.conf' do
-  source 'loadreport-worker.conf.erb'
-  mode 0440
-end
-service "loadreport-worker" do
-	provider Chef::Provider::Service::Upstart
-  action :start
-end
