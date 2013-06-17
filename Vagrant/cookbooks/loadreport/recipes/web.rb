34c8bfa3d7a16fd60e4d74be5845bf073f127488
ryan bridges <ryan.bridges@turner.com>
2013-06-17 00:22:54 -0400

    re-re-re-adding phantomjs

--- Vagrant/cookbooks/loadreport/recipes/web.rb
+++
@@ -1,8 +0,0 @@
-template '/etc/init/loadreport-server.conf' do
-  source 'loadreport-server.conf.erb'
-  mode 0440
-end
-service "loadreport-server" do
-	provider Chef::Provider::Service::Upstart
-  action :start
-end
